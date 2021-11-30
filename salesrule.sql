11fcbe8c44c3ae3056f12bcec85063ca56e088d5
jsinghvirdi <jsingh.virdi@gmail.com>
2021-11-30 11:32:30 +0530

    Create salesrule.sql
    This will remove all sales rules data from database of Magento 2.

---
+++ salesrule.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE `salesrule`;
+TRUNCATE TABLE `salesrule_coupon`;
+TRUNCATE TABLE `salesrule_coupon_aggregated`;
+TRUNCATE TABLE `salesrule_coupon_aggregated_order`;
+TRUNCATE TABLE `salesrule_coupon_aggregated_updated`;
+TRUNCATE TABLE `salesrule_coupon_usage`;
+TRUNCATE TABLE `salesrule_customer`;
+TRUNCATE TABLE `salesrule_customer_group`;
+TRUNCATE TABLE `salesrule_label`;
+TRUNCATE TABLE `salesrule_product_attribute`;
+TRUNCATE TABLE `salesrule_website`;
+
+SET FOREIGN_KEY_CHECKS=1;
